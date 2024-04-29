e754663c0b4eb956ea0726b558d864c767c8469c
Chandan Kumar <146727538+chandan81027@users.noreply.github.com>
2024-04-30 03:21:33 +0530

    Backend Code

---
+++ controller/TodoController.js
@@ -0,0 +1,32 @@
+const TodoModel = require('../models/TodoModels')
+module.exports.getTodo = async (req, res) => {
+    const toDo = await TodoModel.find();
+    res.send(toDo);
+};
+module.exports.saveTodo = async (req, res) => {
+    const { text } = req.body
+    TodoModel.create({ text }) 
+        .then((data) => {
+            console.log('Added Successfully');
+            console.log(data);
+            res.send(data);
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        });
+};
+module.exports.updateTodo= async (req,res)=>{
+    const{_id,text}=req.body;
+    TodoModel.findByIdAndUpdate(_id,{text})
+    .then(()=> res.send("Updated Successfully"))
+    .catch((error)=> console.log(error))
+}
+
+module.exports.deleteTodo= async (req,res)=>{
+    const{_id}=req.body;
+    TodoModel.findByIdAndDelete(_id)
+    .then(()=> res.send("Deleted Successfully"))
+    .catch((error)=> console.log(error))
+}
+
---
+++ routes/TodoRoute.js
@@ -0,0 +1,8 @@
+const {Router}=require('express');
+const { getTodo, saveTodo, updateTodo, deleteTodo } = require('../controller/TodoController');
+const router = Router();
+router.get('/',getTodo)
+router.post('/save',saveTodo)
+router.post('/update',updateTodo)
+router.post('/delete',deleteTodo)
+module.exports= router;
